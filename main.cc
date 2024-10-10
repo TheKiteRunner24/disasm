5e2a066f12bfe2eb63112b46b09dd1571f520521
husipeng <husipeng24@qq.com>
2024-10-10 12:01:13 +0800

    support file input

--- main.cc
+++ main.cc
@@ -5,33 +5,41 @@
 #include <string>
 #include <fstream>
 
-// int main(int argc, char* argv[]) {
-//     std::ifstream infile(argv[1]);
-//     if (!infile) {
-//         std::cerr << "Error opening file!" << std::endl;
-//         return 1;
-//     }
+#ifdef FILE_INPUT
+int main(int argc, char* argv[]) {
+    std::ifstream infile(argv[1]);
+    if (!infile) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
+    }
 
-//     isa_parser_t isa_parser(DEFAULT_ISA, DEFAULT_PRIV);
-//     disassembler_t* disassembler = new disassembler_t(&isa_parser);
+    isa_parser_t isa_parser(DEFAULT_ISA, DEFAULT_PRIV);
+    disassembler_t* disassembler = new disassembler_t(&isa_parser);
 
-//     std::string s;
-//     while (std::getline(infile, s)) {
-//         char* end;
-//         uint64_t insn = strtoull(s.c_str(), &end, 16);
+    std::string s;
+    while (std::getline(infile, s)) {
+        char* end;
+        uint64_t insn;
+        if (strncmp(s.c_str(), "0x", 2) == 0) {
+            // Hexadecimal input
+            insn = strtoull(s.c_str(), &end, 16);
+        } else {
+            // Decimal input
+            insn = strtoull(s.c_str(), &end, 10);
+        }
     
-//         std::string disasm  = disassembler->disassemble(insn);
+        std::string disasm  = disassembler->disassemble(insn);
 
-//         if (disasm == "unknown") {
-//             printf("invalid instruction\n");
-//         } else {
-//             printf("%s\n", disasm.c_str());
-//         }
-//     }
-//     delete disassembler;
-//     return 0;
-// }
-
+        if (disasm == "unknown") {
+            printf("invalid instruction\n");
+        } else {
+            printf("%s\n", disasm.c_str());
+        }
+    }
+    delete disassembler;
+    return 0;
+}
+#else
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <instruction_in_hex>" << std::endl;
@@ -61,3 +69,4 @@ int main(int argc, char* argv[]) {
     delete disassembler;
     return 0;
 }
+#endif
